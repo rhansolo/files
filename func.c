c0f036890479d38953ece081da5c4c3a1699a26e
Fiona Cai <fionacai@Fionas-Air.fios-router.home>
2018-10-23 20:23:22 -0400

    done

--- func.c
+++ func.c
@@ -4,19 +4,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main() 
-{ 
-  char buffer1[20] = "thisisatest\n"; 
-  char buffer2[20]; 
-  
-  char file1 = open("foo.txt", O_RDWR);         
+int main()
+{
+  char buffer1[20] = "thisisatest\n";
+  char buffer2[20];
+
+  char file1 = open("foo.txt", O_RDWR);
   char file2 = open("foo.txt", O_RDWR); 
-  
-  write(file1, buffer1, strlen(buffer1));          
-  write(1, buffer2, read(file2, buffer2, 20)); 
-  
-  close(file1); 
-  close(file2); 
-  
-  return 0; 
-} 
+
+  write(file1, buffer1, strlen(buffer1));
+  write(1, buffer2, read(file2, buffer2, 20));
+
+  close(file1);
+  close(file2);
+
+  return 0;
+}
